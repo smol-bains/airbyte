6edc5f88b499c9c41d0b528ca746eadf8db06cdc
Edward Gao <edward.gao@airbyte.io>
2025-04-23 10:06:46 -0700

    bad merge

--- airbyte-cdk/bulk/toolkits/load-db/src/main/kotlin/io/airbyte/cdk/load/orchestration/db/direct_load_table/DirectLoadInitialStatus.kt
+++ airbyte-cdk/bulk/toolkits/load-db/src/main/kotlin/io/airbyte/cdk/load/orchestration/db/direct_load_table/DirectLoadInitialStatus.kt
@@ -2,14 +2,14 @@
  * Copyright (c) 2025 Airbyte, Inc., all rights reserved.
  */
 
-package io.airbyte.cdk.load.orchestration.direct_load_table
+package io.airbyte.cdk.load.orchestration.db.direct_load_table
 
-import io.airbyte.cdk.load.orchestration.DestinationInitialStatus
+import io.airbyte.cdk.load.orchestration.db.DatabaseInitialStatus
 
 data class DirectLoadInitialStatus(
     val realTable: DirectLoadTableStatus?,
     val tempTable: DirectLoadTableStatus?,
-) : DestinationInitialStatus
+) : DatabaseInitialStatus
 
 data class DirectLoadTableStatus(
     val isEmpty: Boolean,
